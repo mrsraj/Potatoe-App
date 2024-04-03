95ea5705db8d44a655354b891f1cc30b95dd8b48
Sachin Raj <sachinraj6392@gmail.com>
2024-04-03 10:26:46 +0530

    logo

--- src/HeaderLogo.jsx
+++ src/HeaderLogo.jsx
@@ -5,7 +5,7 @@ function LogoDiv() {
         <div className="LogoDiv">
            <img className="LogoImg" src="https://putatoetest.web.app/static/media/logo.aa9da4f6375a03f8abbe.png" alt="COMPANY-LOGO" />
            <section className="logo-text">
-            <h1 className="Logoh1">POTATOE</h1>
+            <h1 className="Logoh1">PUTATOE</h1>
             <span className="Logoh1-lower">One Solution</span>
            </section>
         </div>
